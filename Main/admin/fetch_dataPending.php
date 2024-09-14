d23ead38c657fb1255596b55f49f53de8274eb62
Taufik Hazel <taufikhazel@gmail.com>
2024-09-14 18:07:59 +0700

    1. Responsive 2. ukuran macbook, 1680 3. footer di admin bermasalah 4. Unique di set 5. Fab 6. fetch data di halaman proses (done) 7. handle logika di prosesUser.html untuk mengirim ke proses.html (done)

--- Main/admin/fetch_dataPending.php
+++ Main/admin/fetch_dataPending.php
@@ -3,48 +3,57 @@ $jsonData = file_get_contents('../data/transaksi.json');
 $mobilData = file_get_contents('../data/mobil.json');
 
 $data = json_decode($jsonData, true);
-$dataMobil = json_decode($mobilData, true); 
+$dataMobil = json_decode($mobilData, true);
 
 $mobilMap = [];
-foreach ($dataMobil as $mobil) {
-    $mobilMap[$mobil['idMobil']] = $mobil['namaMobil'];
+if (is_array($dataMobil)) {
+    foreach ($dataMobil as $mobil) {
+        $mobilMap[$mobil['idMobil']] = $mobil['namaMobil'];
+    }
 }
 
 $groupedData = [];
-foreach ($data as $transaction) {
-    if ($transaction['status'] === 'Belum Diproses') {
-        $date = $transaction['submit'];
-        if (!isset($groupedData[$date])) {
-            $groupedData[$date] = [];
+if (is_array($data)) {
+    foreach ($data as $transaction) {
+        if ($transaction['status'] === 'Belum Diproses') {
+            $date = $transaction['submit'];
+            if (!isset($groupedData[$date])) {
+                $groupedData[$date] = [];
+            }
+            $groupedData[$date][] = $transaction;
         }
-        $groupedData[$date][] = $transaction;
     }
 }
 
 krsort($groupedData);
 
 $html = '';
-foreach ($groupedData as $date => $transactions) {
-    $html .= '<div class="separator">';
-    $html .= '<p  class="tanggalBulan">' . $date . '</p>';
-    $html .= '<div class="cardProfile">';
 
-    foreach ($transactions as $transaction) {
-        $idMobil = $transaction['mobil'];
-        $namaMobil = isset($mobilMap[$idMobil]) ? $mobilMap[$idMobil] : 'Unknown Mobil';
+if (empty($groupedData)) {
+    $html = '<p>Data tidak tersedia di database, Belum ada data yang masuk</p>';
+} else {
+    foreach ($groupedData as $date => $transactions) {
+        $html .= '<div class="separator">';
+        $html .= '<p class="tanggalBulan">' . $date . '</p>';
+        $html .= '<div class="cardProfile">';
 
-        $html .= '<div class="leftContent" onclick="saveDataAndRedirect(' . $transaction['idTransaksi'] . ')">';
-        $html .= '<div class="isiContentLeft">';
-        $html .= '<a href="#" class="namaUser">' . $transaction['nama'] . '</a>';
-        $html .= '<a href="#" class="jenisNamaMobil">' . $namaMobil . '</a>';
-        $html .= '<a href="#" class="kodePesanan">Kode Pesanan : ' . $transaction['idTransaksi'] . '</a>';
-        $html .= '<a href="#" class="statusMobil">Status : ' . $transaction['status'] . '</a>';
+        foreach ($transactions as $transaction) {
+            $idMobil = $transaction['mobil'];
+            $namaMobil = isset($mobilMap[$idMobil]) ? $mobilMap[$idMobil] : 'Unknown Mobil';
+
+            $html .= '<div class="leftContent" onclick="saveDataAndRedirect(' . $transaction['idTransaksi'] . ')">';
+            $html .= '<div class="isiContentLeft">';
+            $html .= '<a href="#" class="namaUser">' . $transaction['nama'] . '</a>';
+            $html .= '<a href="#" class="jenisNamaMobil">' . $namaMobil . '</a>';
+            $html .= '<a href="#" class="kodePesanan">Kode Pesanan : ' . $transaction['idTransaksi'] . '</a>';
+            $html .= '<a href="#" class="statusMobil">Status : ' . $transaction['status'] . '</a>';
+            $html .= '</div>';
+            $html .= '</div>';
+        }
+
         $html .= '</div>';
         $html .= '</div>';
     }
-
-    $html .= '</div>';
-    $html .= '</div>'; 
 }
 
 echo $html;
