741b91130eea3dbbcf53fb9d4c821d83585e2920
Taufik Hazel <taufikhazel@gmail.com>
2024-09-14 13:14:20 +0700

    1. Responsive 2. ukuran macbook, 1680 3. footer di admin bermasalah 4. Unique di set 5. Fab 6. fetch data di halaman proses 7. handle logika di prosesUser.html untuk mengirim ke proses.html

---
+++ Main/admin/update_transactions.php
@@ -0,0 +1,25 @@
+<?php
+$dataFile = '../data/transaksi.json';
+
+$transaksiData = json_decode(file_get_contents($dataFile), true);
+
+if ($transaksiData) {
+    $today = new DateTime();
+
+    foreach ($transaksiData as &$transaksi) {
+        if ($transaksi['status'] === "Sedang Diproses") {
+            $transaksiDate = new DateTime($transaksi['tanggal']); 
+
+            if ($today->format('Y-m-d') !== $transaksiDate->format('Y-m-d')) {
+                $transaksi['status'] = "Selesai";
+            }
+        }
+    }
+
+    file_put_contents($dataFile, json_encode($transaksiData, JSON_PRETTY_PRINT));
+    
+    echo json_encode(["status" => "success", "message" => "Transactions updated successfully"]);
+} else {
+    echo json_encode(["status" => "error", "message" => "Failed to read transactions data"]);
+}
+?>
